8d3a1893f3437f56db7b49118b6c7a3e6c071dee
lorem <loremwalker@hotmail.com>
2018-09-17 10:09:38 +0800

    Create hello.cs
    还是把语言加上吧，方便第三方热榜网站获取到此repo

---
+++ Test/hello.cs
@@ -0,0 +1,12 @@
+using System;
+// A "Hello World!" program in C#
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
