97462568f3dc9b348ae357011e5ae834b29e0983
lorem <loremwalker@hotmail.com>
2019-05-04 07:53:53 +0800

    Create helloworld.cs

---
+++ Test/helloworld.cs
@@ -0,0 +1,16 @@
+// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
