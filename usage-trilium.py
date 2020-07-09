2b9f3288aa6e55b86c4408bfd9007bba41eb1ffa
发霉的橄榄果 <hoochanlon@outlook.com>
2020-07-09 11:00:27 +0800

    Update usage-trilium.py

--- usage-trilium.py
+++ usage-trilium.py
@@ -6,4 +6,6 @@
 trilium 上手较为困难，UI存在不能重命名与搜索框无法输入的bug。勾选`delete also all note clones`选项在删除文件的同时，也会删除其对应或相关的功能模块，影响体验使用。
 账号文档数据导入导出也是个问题，似乎并不太适合GitHub+GitJournal，另一个notion与GitHub的关联度太低了，并不适合我的使用。
 
+在得到岛国小伙的[@k7256580-不只是筆記軟體](http://22j.co/bFZX)启发，我还可以用python中文编程来汇总支出呀...jyputer+GitJournal就好了，主要是Free
+
 '''
